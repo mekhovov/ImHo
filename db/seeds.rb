4b3bf6f2a0da7cf6cefa829fb850691b1ef0fe2c
Alex Mekhovov <alex.mehovov@gmail.com>
2011-10-16 17:44:40 +0300

    add photos, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Attach.create :id => 1, :user_id => 1, :photo_file_name => "270_(3x4).jpg", :photo_content_type => "image/jpeg", :photo_file_size => 3927853
+Attach.create :id => 2, :user_id => 1, :photo_file_name => "265_(vkantakt).jpg", :photo_content_type => "image/jpeg", :photo_file_size => 4881065
+Attach.create :id => 3, :user_id => 1, :photo_file_name => "294_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 617276
+Attach.create :id => 4, :user_id => 2, :photo_file_name => "262_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 9818070
+Attach.create :id => 5, :user_id => 1, :photo_file_name => "220_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 4453970
+Attach.create :id => 6, :user_id => 2, :photo_file_name => "189_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 6037097
+Attach.create :id => 7, :user_id => 2, :photo_file_name => "188_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 10983765
+Attach.create :id => 8, :user_id => 2, :photo_file_name => "174_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 6653219
+Attach.create :id => 9, :user_id => 2, :photo_file_name => "164_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 5699707
+Attach.create :id => 10, :user_id => 2, :photo_file_name => "296_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 7168911
+Attach.create :id => 11, :user_id => 2, :photo_file_name => "025_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 4297659
+Attach.create :id => 12, :user_id => 2, :photo_file_name => "035_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 12354128
+Attach.create :id => 13, :user_id => 2, :photo_file_name => "080_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 3819051
+Attach.create :id => 14, :user_id => 2, :photo_file_name => "211_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 10507609
+Attach.create :id => 15, :user_id => 2, :photo_file_name => "027_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 3928326
+Attach.create :id => 16, :user_id => 2, :photo_file_name => "247_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 7154316
+Attach.create :id => 17, :user_id => 1, :photo_file_name => "211_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 4964930
+Attach.create :id => 18, :user_id => 1, :photo_file_name => "278_.jpg", :photo_content_type => "image/jpeg", :photo_file_size => 10380162
